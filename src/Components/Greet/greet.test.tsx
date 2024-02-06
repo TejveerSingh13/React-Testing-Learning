a0beb50cab7dc8de347d3718f361e00755c93bd3
TejveerSingh13 <tejveersingh8513@gmail.com>
2024-02-06 07:42:30 -0700

    Greet Component and Tests

---
+++ src/Components/Greet/greet.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import Greet from "./greet";
+
+// Basic structure , test name, arrow function with virtual DOM and assertion
+test("Greet renders Correctly!", () => {
+  render(<Greet />); // Means the Greet component should render
+  const textELement = screen.getByText(/Greet/i); // And the resulting DOM should have a text greet
+  expect(textELement).toBeInTheDocument();
+});
+
+// Approch for a TDD
+/**
+ * We start with defining the test case
+ * Greet should render the text greet and if a name is passed into the component
+ * It should render hello followed by the name.
+ */
+
+// Here we just wrote the test and then created the component
+
+test("Greet renders Correctly - TDD", () => {
+  render(<Greet />);
+  const textELement = screen.getByText(/Greet/i);
+  expect(textELement).toBeInTheDocument();
+});
+
+test("Greet renders with a name Correctly - TDD", () => {
+  render(<Greet name="Tejveer" />);
+  const textELement = screen.getByText(/Greet Tejveer/i);
+  expect(textELement).toBeInTheDocument();
+});
